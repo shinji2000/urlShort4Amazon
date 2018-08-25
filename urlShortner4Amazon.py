ab8fda2a542faf755fb00ee1737650de45808b7e
shinji2000 <shinji@blacksheep.asia>
2018-08-25 15:05:00 +0900

    add clipboard function

--- urlShortner4Amazon.py
+++ urlShortner4Amazon.py
@@ -6,14 +6,21 @@
 
 import re
 import pyperclip as clip
+from urllib.parse import urlparse
 
-print(clip.paste())
+#print(clip.paste())
 
 def urlShortner():
     text = "https://www.amazon.co.jp/Jupyter-Cookbook-Dan-Toomey/dp/1788839447/ref=sr_1_5?s=books&ie=UTF8&qid=1535164277&sr=1-5&keywords=Jupyter"
 
     if clip.paste():
         text = clip.paste()
+        o = urlparse(text)
+#        print(o.scheme)
+
+        if not (o.scheme == 'http' or o.scheme == 'https') :
+            print("This is not url.")
+            return 1
 
     newUrl = "https://www.amazon.co.jp"
 
@@ -53,17 +60,19 @@ def urlShortner():
         print (newUrl)
         i= i+1
     """
+    if matchObjDp and matchObjRef:
+        i:int = matchObjDp.start()
+        #print("2ndStart:" + str(i) )
+        while i <  matchObjRef.start():
+            newUrl = newUrl + text[i]
+            i= i+1
 
-    i:int = matchObjDp.start()
-    #print("2ndStart:" + str(i) )
-    while i <  matchObjRef.start():
-        newUrl = newUrl + text[i]
-        i= i+1
+        shortUrl = newUrl.replace("www","")
 
-    shortUrl = newUrl.replace("www","")
+        print ("shortUrl:" + shortUrl)
 
-    print ("shortUrl:" + shortUrl)
+        clip.copy(shortUrl)
 
-    clip.copy(shortUrl)
-
+    else:
+        print ("This url is not an introduction page of books on the amazon website.")
 urlShortner()
